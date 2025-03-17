559b70391ee34efd2c2e7ab83c95300d7b93a2e0
mariusz4044 <mariusz4044@gmail.com>
2025-03-17 15:22:12 +0100

    Valud recieve data and user email

--- controllers/register.php
+++ controllers/register.php
@@ -1,15 +1,28 @@
 <?php
 header("Content-Type: application/json");
 
-//init response
-$response = [];
-
-function createErrorResponse(string $message) {
-    $response["status"] = "error";
+function createResponse(string $message, string $status = 'success'): string  {
+    $response = [];
+    $response["status"] = $status;
     $response["message"] = $message;
     return json_encode($response);
 }
 
-$login = $_POST["login"] ?? null;
-$email = $_POST["email"] ?? null;
-$password = $_POST["password"] ?? null;
+$post_data = json_decode(file_get_contents("php://input"), true);
+
+$login = $post_data["login"] ?? null;
+$email = $post_data["email"] ?? null;
+$password = $post_data["password"] ?? null;
+
+if(!$login || !$email || !$password) {
+    echo createResponse("Please fill all the required fields.", "error");
+    exit();
+}
+
+if(!str_contains($email, "@")) {
+    echo createResponse("Please provide valid email address.", "error");
+    exit();
+}
+
+echo createResponse("Register successfully.");
+
